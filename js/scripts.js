9bbaa33a6be67f671c8826f3e74f49567a88f326
Angelica007 <angelicanagahire@gmail.com>
2019-08-05 16:22:17 +0200

    mofied my js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ function myFunction() {
     var x = document.getElementById("myCheck");
     x.checked = true;
   }
+  function Order(size, crusts, toppings) {
+    this.size = size;
+    this.crusts = crusts;
+    this.toppings = toppings;
+  }
+  Order.prototype.fullOrder = function() {
+    return this.size + ", " + this.crusts + ", " + this.toppings;
+  }
